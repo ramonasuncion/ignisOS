7dfaf7c128dbdac10c4e525412b59bf62c5da2ab
Ramon Asuncion <ramon@b1t.dev>
2025-04-14 04:02:26 -0400

    Use linter
    Single comments are broken but most of it looks good.
    
    My only issue is that single lined keywords like bits 16
    with all the spaces between look out of place even though
    they are all lined up

--- src/disk.asm
+++ src/disk.asm
@@ -1,33 +1,33 @@
 ; dl = drive number is set as input to disk_load
 ; es:bx = buffer pointer is set as input as well
 disk_load:
-	push dx
+    push     dx
 
-	mov ah, 0x2						; read mode
-	mov al, dh						; read dh number of sectors
-	mov cl, 0x02					; start from sector 2 (sector 1 is our boot loader)
-	mov ch, 0x00					; cylinder 1
-	mov dh, 0x00					; head 0
+    mov      ah, 0x2                    ; read mode
+    mov      al, dh                     ; read dh number of sectors
+    mov      cl, 0x02                   ; start from sector 2 (sector 1 is our boot loader)
+    mov      ch, 0x00                   ; cylinder 1
+    mov      dh, 0x00                   ; head 0
 
-	int 0x13							; bios interrupt
-	jc disk_error					; check carry bit for error
+    int      0x13                       ; bios interrupt
+    jc       disk_error                 ; check carry bit for error
 
-	pop dx								; original number of sectors to read
-	cmp al, dh						; bios sets al to # of sectors read. error out if not the same.
-	jne disk_error
+    pop      dx                         ; original number of sectors to read
+    cmp      al, dh                     ; bios sets al to # of sectors read. error out if not the same.
+    jne      disk_error
 
-	ret
+    ret
 
 disk_msg:
-    db 'Loaded disk...', 0
+    db       'Loaded disk...', 0
 
 disk_error:
-    mov si, disk_error_msg
-    call print_string
-	jmp $
+    mov      si, disk_error_msg
+    call     print_string
+    jmp      $
 
 disk_error_msg:
-    db 'Disk read error!', 0
+    db       'Disk read error!', 0
 
 ; TODO: https://wiki.osdev.org/Disk_access_using_the_BIOS_(INT_13h)#64-bit_Addressing_Extensions
 
@@ -37,3 +37,4 @@ disk_error_msg:
 ; having problems because of real-mode segment:offset accessing higher memory address and CHS address is limited in general
 
 ; disk_load_ext:
+
