05293e226ec3ffd2b2afb8978448ff2e38b39651
Ramon Asuncion <ramon@b1t.dev>
2025-04-14 20:48:57 -0400

    Update kernel.c to print OK

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,8 @@
-void kernel_main(void)
-{
-  char* video_memory = (char*) 0xb8000;
-  *video_memory = 'X';
+void kernel_main(void) {
+  volatile char *vga = (volatile char*)0xB8000;
+  vga[0] = 'O';
+  vga[1] = 0x0A;
+  vga[2] = 'K';
+  vga[3] = 0x0A;
+  while(1) {}
 }
