0d350ed9494a1bc8f02e13f1edf0fbe51cd79299
Ramon Asuncion <ramon@b1t.dev>
2025-04-18 16:44:52 -0400

    New kmain entry
    I added the entry.asm to find kmain function. I tried
    loaded it at 1 MB but after adding more functions the funciton stack
    moved around the address.

--- src/drivers/vga.c
+++ src/drivers/vga.c
@@ -1,13 +1,33 @@
+#include <drivers/vga.h>
+
+/*
+ * TODO: Move this to header file.
+ */
 volatile char *vga_ptr = (char*)0xB8000;
-unsigned int cursor_offset = 0; /* Each character is 2 bytes. */
+unsigned int cursor_offset = 0; /* each cell is 2 bytes */
+
+#define VGA_COL 80
+#define VGA_ROW 25
+
+void vga_init()
+{
+  vga_clear();
+}
+
+void vga_clear()
+{
+  for (int i = 0; i < VGA_COL * VGA_ROW; ++i)
+    vga_ptr[i] = 0x0000;
+}
 
 void kprint(const char *str)
 {
   unsigned int i = 0;
   while (str[i] != '\0') {
-    vga_ptr[cursor_offset] = str[i++];
+    vga_ptr[cursor_offset] = str[i];
     vga_ptr[cursor_offset + 1] = 0x07; /* color (int color) */
     cursor_offset += 2;
+    i++;
   }
 }
 
--- src/kernel/entry.c
+++ src/kernel/entry.c
@@ -1,15 +1,13 @@
-// #include <drivers/vga.h>
+#include <drivers/vga.h>
 
 void kmain()
 {
-  volatile char *vga_ptr = (char*)0xB8000;
-  vga_ptr[0] = 'O';
-  vga_ptr[1] = 0x0A;
-  vga_ptr[2] = 'K';
-  vga_ptr[3] = 0x0A;
-
-  // kprint("Hello from Ignis!");
-  for (;;) __asm__ volatile("hlt");
+  vga_init();
+  const char str[] = "Hello from Ignis!";
+  // kprint("Hello from Ignis!")
+  // const char *str = "Hello from Ignis!";
+  kprint(str);
+  while (1);
 }
 
 
