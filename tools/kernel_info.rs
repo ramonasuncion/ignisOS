9488c1299b72aae915df82a06d4c0a9f48598a64
Ramon Asuncion <asuncionbatista@gmail.com>
2025-04-22 19:38:34 -0400

    Plan algo matching

--- tools/kernel_info.rs
+++ tools/kernel_info.rs
@@ -5,38 +5,47 @@ fn main() {
     // Run `ls -l build/kernel.bin`
     match run_command("ls", &["-l", "build/kernel.bin"]) {
         Ok(out) => {
+            let size = out.split_whitespace().nth(4).unwrap_or("0");
+            let size: u64 = size.parse().unwrap();
+            println!("Size of kernel.bin: {} bytes", size)
         }
         Err(e) => eprintln!("Error running 'ls': {}", e),
     }
 
-    //// Run `x86_64-elf-readelf -h build/kernel.elf | grep "Entry point"`
+    // Run `x86_64-elf-readelf -h build/kernel.elf | grep "Entry point"`
     match run_command("x86_64-elf-readelf", &["-h", "build/kernel.elf"]) {
         Ok(out) => {
+            out.lines();
+            // break into multiple lines
+            // find the line that has entry point
+            // extract line
+            // extract the entry point
+            println!("{}", out);
         }
         Err(e) => eprintln!("Error running 'x86_64-elf-readelf': {}", e),
     }
 
     // Run `x86_64-elf-nm build/kernel.elf | grep kmain`
     match run_command("x86_64-elf-nm", &["build/kernel.elf"]) {
-        Ok(out) => {
+        Ok(_out) => {
         },
-        Err(e) => eprintln!(""),
+        Err(e) => eprintln!("Error running 'x86_64-elf-nm': {}", e),
     }
 
     // Run `ls -l build/*.bin | awk '{sum += $5} END {print sum, "bytes used"}`
-    match run_command("ls", &["-l", "build/*.bin"]) {
-        Ok(out) => {
-        },
-        Err(e) => eprintln!(""),
-    }
+    //match run_command("ls", &["-l", "build/*.bin"]) {
+    //    Ok(out) => {
+    //    },
+    //    Err(e) => eprintln!(""),
+    //}
 
     // Run `x86_64-elf-nm build/kernel.elf`
-    match run_command("x86_64-elf-nm", &["build/kernel.elf"]) {
-        Ok(out) => {
-            println!("Symbols in kernel.elf:\n{}", out);
-        }
-        Err(e) => eprintln!("Error running 'x86_64-elf-nm': {}", e),
-    }
+    //match run_command("x86_64-elf-nm", &["build/kernel.elf"]) {
+    //    Ok(out) => {
+    //        println!("Symbols in kernel.elf:\n{}", out);
+    //    }
+    //    Err(e) => eprintln!("Error running 'x86_64-elf-nm': {}", e),
+    //}
 }
 
 fn run_command(command: &str, args: &[&str]) -> Result<String, io::Error> {
