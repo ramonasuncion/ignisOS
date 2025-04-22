3d851118a9e54c14a318549e3581b9e35987ab27
Ramon Asuncion <ramon@b1t.dev>
2025-04-22 18:09:58 -0400

    Complete config.sh
    A common script that will be used by other scripts
    to to easily update the directory structure.

--- tools/config.sh
+++ tools/config.sh
@@ -2,12 +2,28 @@
 
 set -eu
 
+[ -n "$($SHELL -c 'echo $BASH_VERSION')" ] && set -o pipefail
+
+[ -n "${VERBOSE}" ]; && set -x
+
+run()
+{
+  if [ -n "${VERBOSE}" ]; then
+   $@
+  else
+   $@ >/dev/null 2>&1
+  fi
+}
+
+export PROJECT_ROOT
 export SRC_DIR
 export BUILD_DIR
 export TOOLS_DIR
+export OS_NAME
 
 PROJECT_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 SRC_DIR="$PROJECT_ROOT/src"
 BUILD_DIR="$PROJECT_ROOT/build"
 TOOLS_DIR="$PROJECT_ROOT/tools"
+OS_NAME="ignis"
 
