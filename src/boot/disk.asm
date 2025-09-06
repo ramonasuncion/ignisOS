4a0632d7fbb5c76dbf5dffd8f80ef971aa0dad4b
Ramon Asuncion <ramon@b1t.dev>
2025-09-06 02:51:53 -0400

    Two steps backwards
      I tthink I narrowed it down to how I load the kernel. I'm working on
      fixing the disk.asm file and seeing if I could load the kernel
      differently.

--- src/boot/disk.asm
+++ src/boot/disk.asm
@@ -1,34 +1,48 @@
-; dl = drive number is set as input to disk_load
-; es:bx = buffer pointer is set as input as well
-; sector 1: Bootloader (stage1)
-; sector 2: Second stage
-; sector 9: Kernel
+; ------------------------------------------------------------------
+; disk_load
+; ------------------------------------------------------------------
+; caller:
+;   DL = drive
+;   DH = head
+;   CH = cylinder
+;   CL = starting sector
+;   AL = number of sectors
+;   ES:BX = buffer
+; ------------------------------------------------------------------
+
+%macro multipop 1-*
+    %rep %0
+        %rotate -1
+        pop %1
+    %endrep
+%endmacro
+
+%macro multipush 1-*
+    %rep %0
+        %rotate -1
+        push %1
+    %endrep
+%endmacro
+
 disk_load:
-    push     dx
+    multipush bx, es
 
-    mov      ah, 0x2                    ; read mode
-    mov      al, dh                     ; read dh number of sectors
-    ; cl is already set by caller (starting sector)
-    mov      ch, 0x00                   ; cylinder 0
-    mov      dh, 0x00                   ; head 0
+    mov    ah, 0x02
+    int    0x13
+    jc     .disk_error
 
-    int      0x13                       ; bios interrupt
-    jc       disk_error                 ; check carry bit for error
-
-    pop      dx                         ; original number of sectors to read
-    cmp      al, dh                     ; bios sets al to # of sectors read. error out if not the same.
-    jne      disk_error
-
+    multipop es, bx
     ret
 
-disk_msg:
-    db       'Loaded disk...', 0
+.disk_error:
+    multipop es, bx
+    mov    si, disk_error_msg
+    call   print_string
+    jmp    $
 
-disk_error:
-    mov      si, disk_error_msg
-    call     print_string
-    jmp      $
+disk_msg:
+    db 'Loaded disk...', 0
 
 disk_error_msg:
-    db       'Disk read error!', 0
+    db 'Disk read error!', 0
 
