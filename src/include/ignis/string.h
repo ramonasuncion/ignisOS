8d8246f0b5aa0bd8bb2b41717d44b77ee82cdab3
Ramon Asuncion <asuncionbatista@gmail.com>
2025-12-26 08:39:49 -0500

    Remove old files for restructure

--- src/include/ignis/string.h
+++
@@ -1,9 +0,0 @@
-#ifndef _IGNIS_STRING_H_
-#define _IGNIS_STRING_H_
-
-#include <stddef.h>
-
-char *itoa(int value, char *str, int base);
-size_t strlen(const char *str);
-
-#endif /* _IGNIS_STRING_H_ */
